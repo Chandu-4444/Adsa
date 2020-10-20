1d4b801c7046c392245122f50644c361f255d3b1
Chandu-4444 <chandrakiran.g19@iiits.in>
2020-10-20 13:02:51 +0530

    Updated Correct Solution Of A$ Q1 (Need Some Modifications!)

--- ADSA/S20190010029_A4.c
+++ ADSA/S20190010029_A4.c
@@ -1,226 +1,265 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-#define COUNT 10
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct TreeNode{
-    int data;
-    char color;
-    struct TreeNode *left;
-    struct TreeNode *right;
-    struct TreeNode *parent;
-}MultiSet;
+// An AVL tree node
+struct node {
+  int key;
+  struct node *left;
+  struct node *right;
+  int height;
+  int count;
+};
+int count = 0, found=0;
 
-
-struct TreeNode **Root; //Duplicate Root
-
-struct TreeNode *Init(int data)
-{
-    struct TreeNode *temp = (struct TreeNode *)malloc(sizeof(struct TreeNode));
-    temp->data = data;
-    temp->color = 'R';
-    temp->left = temp->right = temp->parent = NULL;
-    return temp;
+int height(struct node *N) {
+  if (N == NULL)
+    return 0;
+  return N->height;
 }
 
-void LeftRotate(struct TreeNode **root,struct TreeNode *x)
-{
-    if (!x || !x->right)
-        return ;
-    struct TreeNode *y = x->right;
+int max(int a, int b) { return (a > b) ? a : b; }
 
-    x->right = y->left;
-
-    if (x->right != NULL)
-        x->right->parent = x;
-    y->parent = x->parent;
-
-    if (x->parent == NULL)
-        (*root) = y;
-
-    else if (x == x->parent->left)
-        x->parent->left = y;
-    else    x->parent->right = y;
-
-    y->left = x;
-
-    x->parent = y;
+struct node *newNode(int key) {
+  struct node *node = (struct node *)malloc(sizeof(struct node));
+  node->key = key;
+  node->left = NULL;
+  node->right = NULL;
+  node->height = 1;
+  node->count = 1;
+  return (node);
 }
 
-void RightRotate(struct TreeNode **root,struct TreeNode *y)
-{
-    if (!y || !y->left)
-        return ;
-    struct TreeNode *x = y->left;
-    y->left = x->right;
-    if (x->right != NULL)
-        x->right->parent = y;
-    x->parent =y->parent;
-    if (x->parent == NULL)
-        (*root) = x;
-    else if (y == y->parent->left)
-        y->parent->left = x;
-    else y->parent->right = x;
-    x->right = y;
-    y->parent = x;
+struct node *rightRotate(struct node *y) {
+  struct node *x = y->left;
+  struct node *T2 = x->right;
+
+  x->right = y;
+  y->left = T2;
+
+  y->height = max(height(y->left), height(y->right)) + 1;
+  x->height = max(height(x->left), height(x->right)) + 1;
+
+  return x;
 }
 
+struct node *leftRotate(struct node *x) {
+  struct node *y = x->right;
+  struct node *T2 = y->left;
 
+  y->left = x;
+  x->right = T2;
 
-void FixUp(struct TreeNode **root, struct TreeNode *NewNode)
-{
-    while(NewNode!=*root && NewNode!=(*root)->left && NewNode!=(*root)->right && NewNode->parent->color == 'R' )
-    {
-        struct TreeNode *Uncle;
+  x->height = max(height(x->left), height(x->right)) + 1;
+  y->height = max(height(y->left), height(y->right)) + 1;
 
+  return y;
+}
 
-        //Finding Sibling Of Parent Node (Finding Uncle Node)
-        if(NewNode->parent && NewNode->parent->parent && NewNode->parent == NewNode->parent->parent->left)
-        {
-            Uncle = NewNode->parent->parent->right;
-        }
-        else
-        {
-            Uncle = NewNode->parent->parent->left;
-        }
+int getBalance(struct node *N) {
+  if (N == NULL)
+    return 0;
+  return height(N->left) - height(N->right);
+}
 
-        //If Uncle Is RED,
-        //(i)  Change Color Of Parent And Uncle as BLACK
-        //(ii) Change Color Of GrandParent as RED
-        //(iii) Point NewNode to GrandParent For Repeating The Process Until The Tree Is Fixed
-        if(!Uncle)
-        {
-            NewNode = NewNode->parent->parent;
-        }
-        else if(Uncle->color == 'R')
-        {
-            Uncle->color = 'B';
-            NewNode->parent->color = 'B';
-            NewNode->parent->parent->color = 'R';
-            NewNode = NewNode->parent->parent;
-        }
-        //If Uncle Is BLACK Then, There Are Four Cases (LL, LR, RL, RR)
-        else
-        {
-            //Left-Left (LL) case,
-            // (i) Swap color Of Parent and GrandParent
-            // (ii) Right Rotate GrandParent
-            if(NewNode == NewNode->parent->left && NewNode->parent == NewNode->parent->parent->left && NewNode->parent && NewNode->parent->parent)
-            {
-                //Swapping Colors Of Parent And GrandParent
-                char col = NewNode->parent->color;
-                NewNode->parent->color = NewNode->parent->parent->color;
-                NewNode->parent->parent->color = col;
+struct node *insert(struct node *node, int key) {
+  if (node == NULL)
+    return (newNode(key));
 
-                RightRotate(root ,NewNode->parent->parent);
-            }
-            //Right-Right (RR) Case,
-            // (i) Swap color of Parent and GrandParent
-            // (ii) Left Rotate GrandParent
-            if( NewNode->parent == NewNode->parent->parent->right && NewNode == NewNode->parent->right && NewNode->parent && NewNode->parent->parent)
-            {
-                //Swapping Colors Of Parent And GrandParent
-                char col = NewNode->parent->color;
-                NewNode->parent->color = NewNode->parent->parent->color;
-                NewNode->parent->parent->color = col;
+  if (key == node->key) {
+    (node->count)++;
+    return node;
+  }
 
-                LeftRotate(root, NewNode->parent->parent);
-            }
+  if (key < node->key)
+    node->left = insert(node->left, key);
+  else
+    node->right = insert(node->right, key);
 
-            //Right-Left (RL) case,
-            //(i) Swap Colors of Current Node And GrandParent
-            //(ii) Right Rotate Parent
-            //(iii) Left-Rotate GrandParent
-            if(NewNode->parent && NewNode->parent->parent && NewNode->parent == NewNode->parent->parent->right && NewNode == NewNode->parent->left)
-            {
-                char col = NewNode->color;
-                NewNode->color = NewNode->parent->parent->color;
-                NewNode->parent->parent->color = col;
-                RightRotate(root, NewNode->parent);
-                LeftRotate(root, NewNode->parent->parent);
-            }
+  node->height = max(height(node->left), height(node->right)) + 1;
 
-            //Left-Right (LR) Case,
-            //(i) Swap Color Of Current Node And GrandParent
-            //(ii) Left Rotate Parent
-            //(iii)Right Rotate GrandParent
-            if(NewNode->parent && NewNode->parent->parent && NewNode->parent == NewNode->parent->parent->left && NewNode == NewNode->parent->right)
-            {
-                char col = NewNode->color;
-                NewNode->color = NewNode->parent->parent->color;
-                NewNode->parent->parent->color = col;
-                LeftRotate(root, NewNode->parent);
-                RightRotate(root, NewNode->parent->parent);
-            }
-            
-        }
+  int balance = getBalance(node);
+
+  if (balance > 1 && key < node->left->key)
+    return rightRotate(node);
+
+  if (balance < -1 && key > node->right->key)
+    return leftRotate(node);
+
+  if (balance > 1 && key > node->left->key) {
+    node->left = leftRotate(node->left);
+    return rightRotate(node);
+  }
+
+  if (balance < -1 && key < node->right->key) {
+    node->right = rightRotate(node->right);
+    return leftRotate(node);
+  }
+
+  return node;
+}
+
+struct node *minValueNode(struct node *node) {
+  struct node *current = node;
+
+  while (current->left != NULL)
+    current = current->left;
+
+  return current;
+}
+
+struct node *deleteNode(struct node *root, int key) {
+
+  if (root == NULL)
+    return root;
+
+  if (key < root->key)
+    root->left = deleteNode(root->left, key);
+
+  else if (key > root->key)
+    root->right = deleteNode(root->right, key);
+
+  else {
+
+    if (root->count > 1) {
+      (root->count)--;
+      return root;
     }
-    (*root)->color = 'B'; //Setting Back Color Of Root To Black
+    if ((root->left == NULL) || (root->right == NULL)) {
+      struct node *temp = root->left ? root->left : root->right;
 
+      if (temp == NULL) {
+        temp = root;
+        root = NULL;
+      } else
+        *root = *temp;
+
+      free(temp);
+    } else {
+      struct node *temp = minValueNode(root->right);
+
+      root->key = temp->key;
+      root->count = temp->count;
+      temp->count = 1;
+
+      root->right = deleteNode(root->right, temp->key);
+    }
+  }
+
+  if (root == NULL)
+    return root;
+
+  root->height = max(height(root->left), height(root->right)) + 1;
+
+  int balance = getBalance(root);
+
+  // If this node becomes unbalanced, then there are 4 cases
+
+  if (balance > 1 && getBalance(root->left) >= 0)
+    return rightRotate(root);
+
+  if (balance > 1 && getBalance(root->left) < 0) {
+    root->left = leftRotate(root->left);
+    return rightRotate(root);
+  }
+
+  if (balance < -1 && getBalance(root->right) <= 0)
+    return leftRotate(root);
+
+  if (balance < -1 && getBalance(root->right) > 0) {
+    root->right = rightRotate(root->right);
+    return leftRotate(root);
+  }
+
+  return root;
 }
 
-void Insert(struct TreeNode **root, int data)
-{
-    struct TreeNode * NewNode;
-    NewNode = Init(data);
-    if(*root == NULL)
-    {
-        NewNode->color = 'B';
-        (*root) = NewNode;
-        Root = root;
+void InOrder(struct node *root) {
+  if (root != NULL) {
+    InOrder(root->left);
+    for (int i = 0; i < root->count; i++)
+      printf("%d ", root->key);
+    InOrder(root->right);
+  }
+}
+
+void Frequency(struct node *root, int val) {
+  if (root != NULL) {
+    Frequency(root->left, val);
+    if (root->key == val) {
+      count = root->count;
     }
-    else
-    {
-        struct TreeNode *NewNodeParent = NULL;
-        struct TreeNode *CurrRoot =(*root);
-
-        //Following Standard BST INSERTION Steps
-        while (CurrRoot)
-        {
-            NewNodeParent = CurrRoot;
-            if(NewNode->data < CurrRoot->data)
-            {
-                CurrRoot = CurrRoot->left;
-            }
-            else
-            {
-                CurrRoot = CurrRoot->right;
-            }
-        }
-        //By The Time We Reach Here, We Have Found The Correct Place Of Element To Be Inserted.
-        NewNode->parent = NewNodeParent;
-        if(NewNode->data > NewNodeParent->data)
-        {
-            NewNodeParent->right = NewNode; 
-        }
-        else
-        {
-            NewNodeParent->left = NewNode; 
-        }
-
-        FixUp(root, NewNode);
-        
-    }
-
+    Frequency(root->right, val);
+  }
 }
 
 
-int main()
-{
-    MultiSet *root=NULL;
-    printf("Start Entering Node : \n");
-    while(1)
-    {
-        int val;
-        scanf("%d",&val);
-        Insert(&root, val);
-        if(val==-1)
-        {
-            break;
-        }
+
+
+void Select(struct node *root, int val) {
+  if (root) {
+    Select(root->left, val);
+    if (count == val) {
+      printf("The Element In Position %d Is: %d\n", val, root->key);
+      found =1;
     }
-    printf("\n");
-   
+    count++;
+    Select(root->right, val);
+  }
 }
 
+int main() {
+  struct node *root = NULL;
+  int choice, val;
+  printf("\t0. Exit\n\t1. Insert(M, i)\n\t2. Remove(M, i)\n\t3. Frequency(M, "
+         "i)\n\t4. Select(M, i)\n");
+  printf("Enter Your Choice: ");
+  scanf("%d", &choice);
+  while (1) {
+    switch (choice) {
+    case 1:
+      printf("Enter Value To Be Inserted: ");
+      scanf("%d", &val);
+      root = insert(root, val);
+      InOrder(root);
+      printf("\n");
+      break;
+    case 2:
+      printf("Enter The Value To Be Removed: ");
+      scanf("%d", &val);
+      root = deleteNode(root, val);
+      InOrder(root);
+      printf("\n");
+      break;
+    case 3:
+      printf("Enter The To Find Its Frequency: ");
+      scanf("%d", &val);
+      Frequency(root, val);
+      printf("The Frequency Of %d Is: %d\n", val, count);
+      count = 0;
+      break;
+    case 4:
+      printf("Enter The Position: ");
+      scanf("%d", &val);
+      InOrder(root);
+      printf("\n");
+      Select(root, val);
+      if(found==0)
+      {
+          printf("Set Has No Element At Index: %d\n", val);
+      }
+      count = 0;
+      break;
 
+    default:
+      printf("Wrong Choice : %d!\n", choice);
+      break;
+    }
+    printf("\t0. Exit\n\t1. Insert(M, i)\n\t2. Remove(M, i)\n\t3. Frequency(M, "
+           "i)\n\t4. Select(M, i)\n");
+    printf("Enter Your Choice: ");
+    scanf("%d", &choice);
+  }
+
+  printf("\n");
+  return 0;
+}
